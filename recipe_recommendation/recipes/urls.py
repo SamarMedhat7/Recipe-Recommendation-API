3494880e07f0bda24b4d91884e548d3c9887118c
SamarMedhat7 <samarmedhat573@gmail.com>
2025-01-12 10:59:06 +0200

    User Registration

--- recipe_recommendation/recipes/urls.py
+++ recipe_recommendation/recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RecipeListView, RecipeRecommendationView
+from .views import RecipeListView, RecipeRecommendationView ,RegisterUserView
 
 urlpatterns = [
     path('recipes/', RecipeListView.as_view(), name='recipe-list'),
     path('recommend/', RecipeRecommendationView.as_view(), name='recipe-recommend'),
+    path('register/', RegisterUserView.as_view(), name='register'),
 ]
--- recipe_recommendation/recipes/views.py
+++ recipe_recommendation/recipes/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from .models import Recipe, Ingredient
 from .serializers import RecipeSerializer
 from rapidfuzz import fuzz, process
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework import status, generics
+from rest_framework.permissions import IsAuthenticated
 
 class RecipeListView(APIView):
     def get(self, request):
@@ -32,4 +36,24 @@ class RecipeRecommendationView(APIView):
                 })
 
         return Response(suggestions)
+    
 
+
+# --- User Management Views ---
+class RegisterUserView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+
+        if not username or not password or not email:
+            return Response({"error": "All fields are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Username already exists."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, password=password, email=email)
+        token = Token.objects.create(user=user)
+        return Response({"message": "User registered successfully.", "token": token.key})
+    
+   
